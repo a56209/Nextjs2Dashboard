a208fda20e15314a84373fab32859c33287f2005
A56209 <41112394@qq.com>
2026-05-31 22:39:52 +0800

    身份认证

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
@@ -70,4 +73,24 @@ export async function deleteInvoice(id: string) {
   
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type){
+        case 'CredentialsSignin':
+          return 'Invalid email or password.';
+        default:
+          return 'An error occurred during authentication.';
+      }      
+    }
+    throw error;
+  }
+  
 }
---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import { match } from "assert";
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  runtime : "nodejs",
+}
+
